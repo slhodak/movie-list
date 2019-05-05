be0a94836d114944f293dec482cb4d6d29d4a5ab
Sam Hodak <slhodak@gmail.com>
2019-05-05 13:45:30 -0700

    Enable search filtering of list

--- src/components/app.js
+++ src/components/app.js
@@ -2,27 +2,36 @@ import React from 'react';
 import MovieList from './movieList';
 import SearchBar from './search';
 import movies from '../data/movieData';
+import _ from 'lodash';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: movies
+      movies: movies,
+      matches: []
     }
     this.searchList = this.searchList.bind(this);
   }
 
   searchList(input) {
-    let found = false;
+    let matches = [];
     this.state.movies.forEach((movie) => {
       movie.title.toLowerCase().split(' ').forEach((word) => {
         if (word === input.toLowerCase().trim()) {
-          found = true;
+          matches.push(movie);
         }
       });
     });
-    console.log(found);
-    return found;
+    let noResultsElement = document.getElementById('noResults');
+    if (!matches.length && input.length) {
+      noResultsElement.hidden = false;
+    } else {
+      noResultsElement.hidden = true;
+    }
+    this.setState({
+      matches: matches
+    });
   }
 
   render() {
@@ -30,7 +39,10 @@ class App extends React.Component {
       <div>
         <h1 className="movieListHeader">Movie List</h1>
         <SearchBar search={this.searchList} />
-        <MovieList movies={this.state.movies} />
+        <h2 id="noResults" hidden={true}>Sorry, no results were found</h2>
+        <MovieList movies={this.state.movies.filter((movie) => {
+          return this.state.matches.length === 0 || _.includes(this.state.matches, movie);
+        })} />
       </div>
     )
   }
